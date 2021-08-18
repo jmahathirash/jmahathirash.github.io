d7b3f18c2bc13bc32b5d19aa28ba13d3d9c13038
jmahathirash <87577842+jmahathirash@users.noreply.github.com>
2021-08-17 20:33:16 -0400

    updates to app.js, index.html and style.css
    updates

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,82 @@
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
+const scoreboard = {
+    player: 0,
+    computer: 0
+};
+
+function getCompChoice() {
+    const randNum = Math.random();
+    if (randNum < 0.34) {
+        return 'rock';
+    } else if (randNum <= 0.67) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+function play(userChoice) {
+    const compChoice = getCompChoice();
+    const win = getWinner(userChoice, compChoice);
+
+    showWinner(win, compChoice)
+}
+
+function getWinner(p, c) {
+    if (p === c) {
+        return 'draw';
+    } else if (p === 'rock') {
+        if(c === 'paper') {
+            return 'computer';
+        } else {
+            return 'player';
+        }       
+
+    } else if(p === 'paper') {
+        if(c === 'scissors') {
+            return 'computer';
+        }else {
+            return 'player';
+        }
+
+    } else if(p === 'scissors') {
+        if(c === 'rock') {
+            return 'computer'
+        } else {
+            return 'player'
+        }
+    }
+};
+
+function showWinner(win, compChoice) {
+    if(win === 'player') {
+        scoreboard.player++;
+    } else if(win === 'computer') {
+        scoreboard.computer++;
+    }
+
+    score.innerHTML = `
+        <p>Player ${scoreboard.player}</p>
+        <p>Computer: ${scoreboard.computer}</p>
+        `;
+}
+
+function main() {
+    paperDiv.addEventListener('click', function() {
+        play("paper");
+    })
+    
+    rockDiv.addEventListener('click', function() {
+        play("rock");
+    })
+    
+    scissorsDiv.addEventListener('click', function() {
+        play("scissors");
+    })
+};
+main();
